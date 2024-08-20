acdb86717fa83a3a748142415a5f60e204660ef5
kritanjan-polley <104536210+kritanjan-polley@users.noreply.github.com>
2024-08-20 11:32:47 -0700

    Update run.sh

--- spinBoson/HEOM/run.sh
+++ spinBoson/HEOM/run.sh
@@ -5,7 +5,7 @@ if test -f "$FILE"; then
     rm a.out
 fi
 
-# gfortran -fcheck=all -fbacktrace -Wall -Wextra heom4.f95 spectra_heom4.f95 -fopenmp
+# gfortran -fcheck=all -fbacktrace -Wall -Wextra heom4.f90 spectra_heom4.f90 -fopenmp
 gfortran -O3 -ffast-math -funroll-loops heom4.f90 spectra_heom4.f90 -fopenmp
 ./a.out 
 rm a.out *.mod
