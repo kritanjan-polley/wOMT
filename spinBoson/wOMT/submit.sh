836180cbd67a26e2b55edb5d1525ba071cf0e191
kritanjan-polley <104536210+kritanjan-polley@users.noreply.github.com>
2024-08-21 19:15:42 -0700

    Update submit.sh

--- spinBoson/wOMT/submit.sh
+++ spinBoson/wOMT/submit.sh
@@ -5,7 +5,7 @@ if test -f "$FILE"; then
     rm a.out
 fi
 
-gfortran -O3 -ffast-math -funroll-loops params.f95 functions.f95 propagator.f95 main.f95 -fopenmp
+gfortran -O3 -ffast-math -funroll-loops params.f90 functions.f90 propagator.f90 main.f90 -fopenmp
 ./a.out
 rm a.out
 rm *.mod
